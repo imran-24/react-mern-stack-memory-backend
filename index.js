6100bc8b59dda3358aa1107ecac94ff4040e1fe8
joe-nomad <1921731@iub.edu.bd>
2023-01-20 04:25:02 +0600

    final fix link

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json({limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true }))
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:3000", "https://share-memory.onrender.com"],
+  origin: ["http://localhost:3000", "https://mern-share-memory-api.onrender.com/"],
 }));
 
 app.use("/api/memory", memoryRoutes);
